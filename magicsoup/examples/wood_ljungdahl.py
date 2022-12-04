fea4cc1126c61ade1395fdea7c6bf96cd119f6f8
Marc Schwering <schweringmarc01@gmail.com>
2022-12-04 14:51:51 +0100

    setting up new dynamics and genetics

---
+++ magicsoup/examples/wood_ljungdahl.py
@@ -0,0 +1,14 @@
+## Normal (https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2646786/)
+#
+# Methyl (Eastern) Branch:
+# formate dehydrogenase (1.2.1.43): CO2 + NADPH -> formiat + NADP (+ 21.5 kJ/mol)
+# formate-tetrahydrofolate ligase (6.3.4.3): formiat + FH4 + ATP -> formyl-FH4 + ADP + Pi (−8.4 kJ/mol)
+# methenyltetrahydrofolate cyclohydrolase (3.5.4.9): formyl-FH4 + NADPH -> methylen-FH4 + NADP (-40.2 kJ/mol)
+# methylenetetrahydrofolate reductase (1.1.99.15): methylen-FH4 + NADPH -> methyl-FH4 + NADP (-39.2 kJ/mol)
+#
+# Carbonyl (Western) Branch:
+# CFeSP methyltransferase (2.1.1.X): methyl-FH4 + Ni-ACS -> FH4 + methyl-Ni-ACS
+# CO dehydrogenase / acetyl-CoA synthetase: CO2 + HS-CoA + methyl-Ni-ACS -> Ni-ACS + acetyl-CoA
+
+# CO-methylating acetyl-CoA synthase (2.3.1.169): acetyl-CoA
+
