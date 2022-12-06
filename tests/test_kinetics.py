3a8fcb684306d37749a997c4ded68f85c540eee1
Marc Schwering <schweringmarc01@gmail.com>
2022-12-06 21:59:04 +0100

    negative concentration adjustment works

--- tests/test_kinetics.py
+++ tests/test_kinetics.py
@@ -3,7 +3,7 @@ import torch
 
 TOLERANCE = 1e-4
 
-# TODO: masked tensor (pytorch.org/docs/stable/masked.html)
+# TODO: masked tensors? (pytorch.org/docs/stable/masked.html)
 
 
 def f(
@@ -47,114 +47,26 @@ def f(
     # concentration deltas (c, s)
     Xd = torch.einsum("cps,cp->cs", Z, prot_V)
 
+    X1 = X + Xd
+    if torch.any(X1 < 0):
+        neg_conc = torch.where(X1 < 0, 1.0, 0.0)  # (c, s)
+        candidates = torch.where(Z < 0.0, 1.0, 0.0)  # (c, p, s)
+
+        # which proteins need to be down-regulated (c, p)
+        BX_mask = torch.einsum("cps,cs->cps", candidates, neg_conc)
+        prot_M = BX_mask.max(dim=2).values
+
+        # what are the correction factors (c,)
+        correct = torch.where(neg_conc > 0.0, -X / Xd, 1.0).min(dim=1).values
+
+        # correction for protein velocities (c, p)
+        prot_V_adj = torch.einsum("cp,c->cp", prot_M, correct)
+        prot_V_adj = torch.where(prot_V_adj == 0.0, 1.0, prot_V_adj)
+
+        # new concentration deltas (c, s)
+        Xd = torch.einsum("cps,cp->cs", Z, prot_V * prot_V_adj)
+
     return Xd
-
-
-def test_mm_kinetic_with_allosteric_action():
-    # 2 cell, 3 max proteins, 4 molecules (a, b, c, d)
-    # cell 0: P0: a -> b, inhibitor=c, P1: c -> d, activator=a
-    # cell 1: P0: a -> b, inhibitor=c,d, P1: c -> d, activator=a,b
-
-    # fmt: off
-
-    # concentrations (c, s)
-    X0 = torch.tensor([
-        [1.1, 2.5, 0.9, 1.0],
-        [2.3, 1.6, 3.0, 0.9],
-    ])
-
-    # reactions (c, p, s)
-    Z = torch.tensor([
-        [   [-1.0, 1.0, 0.0, 0.0],
-            [0.0, 0.0, -1.0, 1.0],
-            [0.0, 0.0, 0.0, 0.0]   ],
-        [   [-1.0, 1.0, 0.0, 0.0],
-            [0.0, 0.0, -1.0, 1.0],
-            [0.0, 0.0, 0.0, 0.0]   ],
-    ])
-
-    # affinities (c, p, s)
-    K = torch.tensor([
-        [   [1.2, 3.1, 0.7, 0.0],
-            [1.0, 0.0, 0.9, 1.2],
-            [0.0, 0.0, 0.0, 0.0] ],
-        [   [2.1, 1.3, 1.0, 0.6],
-            [1.3, 0.8, 0.3, 1.1],
-            [0.0, 0.0, 0.0, 0.0] ],
-    ])
-
-    # max velocities (c, p)
-    V = torch.tensor([
-        [2.1, 2.0, 0.0],
-        [3.2, 2.5, 0.0],
-    ])
-
-    # allosterics (c, p, s)
-    A = torch.tensor([
-        [   [0.0, 0.0, -1.0, 0.0],
-            [1.0, 0.0, 0.0, 0.0],
-            [0.0, 0.0, 0.0, 0.0]   ],
-        [   [0.0, 0.0, -1.0, -1.0],
-            [0.0, 0.0, 1.0, 1.0],
-            [0.0, 0.0, 0.0, 0.0]   ],
-    ])
-    # fmt: on
-
-    def mmi(x, kx, v, i, ki):
-        return v * x / (kx + x) * (1 - i / (ki + i))
-
-    def mm2i(x, kx, v, i1, ki1, i2, ki2):
-        return v * x / (kx + x) * max(0, 1 - i1 / (ki1 + i1) - i2 / (ki2 + i2))
-
-    def mma(x, kx, v, a, ka):
-        return v * x / (kx + x) * a / (ka + a)
-
-    def mm2a(x, kx, v, a1, ka1, a2, ka2):
-        return v * x / (kx + x) * min(1, a1 / (ka1 + a1) + a2 / (ka2 + a2))
-
-    # expected outcome
-    v0_c0 = mmi(x=X0[0, 0], v=V[0, 0], kx=K[0, 0, 0], i=X0[0, 2], ki=K[0, 0, 2])
-    v1_c0 = mma(x=X0[0, 2], v=V[0, 1], kx=K[0, 1, 2], a=X0[0, 0], ka=K[0, 1, 0])
-    dx_c0_b = v0_c0
-    dx_c0_a = -v0_c0
-    dx_c0_c = -v1_c0
-    dx_c0_d = v1_c0
-
-    v0_c1 = mm2i(
-        x=X0[1, 0],
-        v=V[1, 0],
-        kx=K[1, 0, 0],
-        i1=X0[1, 2],
-        ki1=K[1, 0, 2],
-        i2=X0[1, 3],
-        ki2=K[1, 0, 3],
-    )
-    v1_c1 = mm2a(
-        x=X0[1, 2],
-        v=V[1, 1],
-        kx=K[1, 1, 2],
-        a1=X0[1, 0],
-        ka1=K[1, 1, 0],
-        a2=X0[1, 1],
-        ka2=K[1, 1, 1],
-    )
-    dx_c1_a = -v0_c1
-    dx_c1_b = v0_c1
-    dx_c1_c = -v1_c1
-    dx_c1_d = v1_c1
-
-    # test
-    Xd = f(X=X0, K=K, V=V, Z=Z, A=A)
-
-    assert Xd[0, 0] == pytest.approx(dx_c0_a, abs=TOLERANCE)
-    assert Xd[0, 1] == pytest.approx(dx_c0_b, abs=TOLERANCE)
-    assert Xd[0, 2] == pytest.approx(dx_c0_c, abs=TOLERANCE)
-    assert Xd[0, 3] == pytest.approx(dx_c0_d, abs=TOLERANCE)
-
-    assert Xd[1, 0] == pytest.approx(dx_c1_a, abs=TOLERANCE)
-    assert Xd[1, 1] == pytest.approx(dx_c1_b, abs=TOLERANCE)
-    assert Xd[1, 2] == pytest.approx(dx_c1_c, abs=TOLERANCE)
-    assert Xd[1, 3] == pytest.approx(dx_c1_d, abs=TOLERANCE)
 
 
 def test_simple_mm_kinetic():
@@ -373,3 +285,192 @@ def test_mm_kinetic_with_multiple_substrates():
     assert Xd[1, 2] == pytest.approx(dx_c1_c, abs=TOLERANCE)
     assert Xd[1, 3] == pytest.approx(dx_c1_d, abs=TOLERANCE)
 
+
+def test_mm_kinetic_with_allosteric_action():
+    # 2 cell, 3 max proteins, 4 molecules (a, b, c, d)
+    # cell 0: P0: a -> b, inhibitor=c, P1: c -> d, activator=a
+    # cell 1: P0: a -> b, inhibitor=c,d, P1: c -> d, activator=a,b
+
+    # fmt: off
+
+    # concentrations (c, s)
+    X0 = torch.tensor([
+        [1.1, 2.5, 0.9, 1.0],
+        [2.3, 1.6, 3.0, 0.9],
+    ])
+
+    # reactions (c, p, s)
+    Z = torch.tensor([
+        [   [-1.0, 1.0, 0.0, 0.0],
+            [0.0, 0.0, -1.0, 1.0],
+            [0.0, 0.0, 0.0, 0.0]   ],
+        [   [-1.0, 1.0, 0.0, 0.0],
+            [0.0, 0.0, -1.0, 1.0],
+            [0.0, 0.0, 0.0, 0.0]   ],
+    ])
+
+    # affinities (c, p, s)
+    K = torch.tensor([
+        [   [1.2, 3.1, 0.7, 0.0],
+            [1.0, 0.0, 0.9, 1.2],
+            [0.0, 0.0, 0.0, 0.0] ],
+        [   [2.1, 1.3, 1.0, 0.6],
+            [1.3, 0.8, 0.3, 1.1],
+            [0.0, 0.0, 0.0, 0.0] ],
+    ])
+
+    # max velocities (c, p)
+    V = torch.tensor([
+        [2.1, 2.0, 0.0],
+        [3.2, 2.5, 0.0],
+    ])
+
+    # allosterics (c, p, s)
+    A = torch.tensor([
+        [   [0.0, 0.0, -1.0, 0.0],
+            [1.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0]   ],
+        [   [0.0, 0.0, -1.0, -1.0],
+            [0.0, 0.0, 1.0, 1.0],
+            [0.0, 0.0, 0.0, 0.0]   ],
+    ])
+    # fmt: on
+
+    def mmi(x, kx, v, i, ki):
+        return v * x / (kx + x) * (1 - i / (ki + i))
+
+    def mm2i(x, kx, v, i1, ki1, i2, ki2):
+        return v * x / (kx + x) * max(0, 1 - i1 / (ki1 + i1) - i2 / (ki2 + i2))
+
+    def mma(x, kx, v, a, ka):
+        return v * x / (kx + x) * a / (ka + a)
+
+    def mm2a(x, kx, v, a1, ka1, a2, ka2):
+        return v * x / (kx + x) * min(1, a1 / (ka1 + a1) + a2 / (ka2 + a2))
+
+    # expected outcome
+    v0_c0 = mmi(x=X0[0, 0], v=V[0, 0], kx=K[0, 0, 0], i=X0[0, 2], ki=K[0, 0, 2])
+    v1_c0 = mma(x=X0[0, 2], v=V[0, 1], kx=K[0, 1, 2], a=X0[0, 0], ka=K[0, 1, 0])
+    dx_c0_b = v0_c0
+    dx_c0_a = -v0_c0
+    dx_c0_c = -v1_c0
+    dx_c0_d = v1_c0
+
+    v0_c1 = mm2i(
+        x=X0[1, 0],
+        v=V[1, 0],
+        kx=K[1, 0, 0],
+        i1=X0[1, 2],
+        ki1=K[1, 0, 2],
+        i2=X0[1, 3],
+        ki2=K[1, 0, 3],
+    )
+    v1_c1 = mm2a(
+        x=X0[1, 2],
+        v=V[1, 1],
+        kx=K[1, 1, 2],
+        a1=X0[1, 0],
+        ka1=K[1, 1, 0],
+        a2=X0[1, 1],
+        ka2=K[1, 1, 1],
+    )
+    dx_c1_a = -v0_c1
+    dx_c1_b = v0_c1
+    dx_c1_c = -v1_c1
+    dx_c1_d = v1_c1
+
+    # test
+    Xd = f(X=X0, K=K, V=V, Z=Z, A=A)
+
+    assert Xd[0, 0] == pytest.approx(dx_c0_a, abs=TOLERANCE)
+    assert Xd[0, 1] == pytest.approx(dx_c0_b, abs=TOLERANCE)
+    assert Xd[0, 2] == pytest.approx(dx_c0_c, abs=TOLERANCE)
+    assert Xd[0, 3] == pytest.approx(dx_c0_d, abs=TOLERANCE)
+
+    assert Xd[1, 0] == pytest.approx(dx_c1_a, abs=TOLERANCE)
+    assert Xd[1, 1] == pytest.approx(dx_c1_b, abs=TOLERANCE)
+    assert Xd[1, 2] == pytest.approx(dx_c1_c, abs=TOLERANCE)
+    assert Xd[1, 3] == pytest.approx(dx_c1_d, abs=TOLERANCE)
+
+
+def test_reduce_velocity_to_avoid_negative_concentrations():
+    # 2 cell, 3 max proteins, 4 molecules (a, b, c, d)
+    # cell 0: P0: a -> b, P1: b -> d
+    # cell 1: P0: 2c -> d
+
+    # fmt: off
+
+    # concentrations (c, s)
+    X0 = torch.tensor([
+        [0.1, 0.1, 2.9, 0.8],
+        [2.9, 3.1, 0.1, 1.0],
+    ])
+
+    # reactions (c, p, s)
+    Z = torch.tensor([
+        [   [-1.0, 1.0, 0.0, 0.0],
+            [0.0, -1.0, 0.0, 1.0],
+            [0.0, 0.0, 0.0, 0.0]    ],
+        [   [0.0, 0.0, -2.0, 1.0],
+            [0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0]    ],
+    ])
+
+    # affinities (c, p, s)
+    K = torch.tensor([
+        [   [1.2, 1.5, 0.0, 0.0],
+            [0.0, 0.5, 0.0, 3.5],
+            [0.0, 0.0, 0.0, 0.0] ],
+        [   [0.0, 0.0, 0.5, 1.5],
+            [0.0, 0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0, 0.0] ],
+    ])
+
+    # max velocities (c, p)
+    V = torch.tensor([
+        [2.1, 1.0, 0.0],
+        [3.1, 0.0, 0.0],
+    ])
+
+    # allosterics (c, p, s)
+    A = torch.zeros(2, 3, 4)
+    # fmt: on
+
+    def mm(x, k, v, n=1):
+        return v * x ** n / (k + x) ** n
+
+    # expected outcome
+    v0_c0 = mm(x=X0[0, 0], v=V[0, 0], k=K[0, 0, 0])
+    # but this would lead to Xd + X0 = -0.0615 (for a)
+    assert X0[0, 0] - v0_c0 < 0.0
+    # so velocity should be reduced to a (it will be 0 afterwards)
+    v0_c0 = X0[0, 0]
+    # the other protein was unaffected
+    v1_c1 = mm(x=X0[0, 1], v=V[0, 1], k=K[0, 1, 1])
+    dx_c0_a = -v0_c0
+    dx_c0_b = v0_c0 - v1_c1
+    dx_c0_c = 0.0
+    dx_c0_d = v1_c1
+
+    v0_c1 = mm(x=X0[1, 2], v=V[1, 0], k=K[1, 0, 2], n=2)
+    # but this would lead to Xd + X0 = -0.0722 (for c)
+    assert X0[1, 2] - 2 * v0_c1 < 0.0
+    # so velocity should be reduced to c (it will be 0 afterwards)
+    v0_c1 = X0[1, 2] / 2
+    dx_c1_a = 0.0
+    dx_c1_b = 0.0
+    dx_c1_c = -2 * v0_c1
+    dx_c1_d = v0_c1
+
+    # test
+    Xd = f(X=X0, K=K, V=V, Z=Z, A=A)
+
+    assert Xd[0, 0] == pytest.approx(dx_c0_a, abs=TOLERANCE)
+    assert Xd[0, 1] == pytest.approx(dx_c0_b, abs=TOLERANCE)
+    assert Xd[0, 2] == pytest.approx(dx_c0_c, abs=TOLERANCE)
+    assert Xd[0, 3] == pytest.approx(dx_c0_d, abs=TOLERANCE)
+
+    assert Xd[1, 0] == pytest.approx(dx_c1_a, abs=TOLERANCE)
+    assert Xd[1, 1] == pytest.approx(dx_c1_b, abs=TOLERANCE)
+    assert Xd[1, 2] == pytest.approx(dx_c1_c, abs=TOLERANCE)
+    assert Xd[1, 3] == pytest.approx(dx_c1_d, abs=TOLERANCE)
