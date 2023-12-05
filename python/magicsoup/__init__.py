38747a51f28ede04e53cd622ff12eb95ab64957b
Marc Schwering <schweringmarc01@gmail.com>
2023-12-05 11:46:33 +0100

    v0.10.0

--- python/magicsoup/__init__.py
+++ python/magicsoup/__init__.py
@@ -1,5 +1,3 @@
-__version__ = "0.9.0"
-
 from .containers import *
 from .genetics import *
 from .world import *
--- scripts/build-and-release.sh
+++ scripts/build-and-release.sh
@@ -10,9 +10,5 @@
 
 set -e
 
-version=$(head -n 1 ./python/magicsoup/__init__.py)
-read -p "Build and release as ${version}? (Y/N)" confirm
-[[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-
 python -m build .
 python -m twine upload --skip-existing dist/*
