e8d83f88b8089b9153df9a811a298fc3eb0488eb
Marc Schwering <schweringmarc01@gmail.com>
2023-12-05 12:01:59 +0100

    add maturin

---
+++ rust/lib.rs
@@ -0,0 +1,14 @@
+use pyo3::prelude::*;
+
+extern crate pyo3;
+
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+#[pymodule]
+fn _lib(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
+}
